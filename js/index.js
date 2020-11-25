2a895c7622f3131e5b792610c1bb4d8dd9228af6
Lindsay Morris <lindsaymorris813@gmail.com>
2020-11-24 21:41:20 -0500

    added questions

--- js/index.js
+++ js/index.js
@@ -26,10 +26,29 @@ inquirer
             name: 'contribute',
             type: 'input',
             message: 'How can people contribute to your project?',
-        }
+        },
         {
-            name: '',
-            type: '',
-            message: '',
-        }
+            name: 'tests',
+            type: 'input',
+            message: 'Describe the tests for your application, and how to use them:',
+        },
+        {
+            name: 'license',
+            type: 'list',
+            message: 'Choose the type of license for your project:',
+            choices: ['MIT', 'GPL 3.0', 'Apache 2.0', 'GPL 2.0', 'BSD 3', 'LGPL 2.1', 'Ms-Pl', 'BSD 2'],
+        },
+        {
+            name: 'gitHub',
+            type: 'input', 
+            message: 'What is your gitHub username?',
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: 'What is your email address?',
+        },
     ])
+    .then((response => {
+
+    }))
